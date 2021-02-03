92be50866f9ff0d1ebfe7796ac0a864a4bcd82e3
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-03 20:35:41 +0530

    Added the code to calculate Daily Wage along with the Total Wage.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,6 +8,10 @@ isFullTime=2;
 No_of_working_days=20
 max_hours=100;
 
+
+total_working_days=0;
+total_work_hours=0;
+
 function getWorkingHours() {
 	case $1 in
 		$isFullTime)
@@ -23,12 +27,21 @@ function getWorkingHours() {
 	echo $emp_hour
 }
 
+function dailyWage() {
+        local empHours=$1
+        dailyWage=$(( $empHours*$wage_per_hour ))
+        echo $dailyWage
+}
+
 while [[ $total_working_days -lt $No_of_working_days && $total_work_hours -lt $max_hours  ]]
 do
 	((total_working_days++))
 	emp_hour="$( getWorkingHours $((RANDOM%3)) )"
 	total_work_hours=$(($total_work_hours+$emp_hour))
+	dailyWage[$total_working_days]="$( dailyWage $emp_hour )"
+	total_salary=$(( $total_work_hours * $wage_per_hour ))
+
 done
 
-total_salary=$(( $total_work_hours * $wage_per_hour ))
 echo "Monthly Wage of an Employee is : " $total_salary
+echo "Daily Wage of an Employee is : " ${dailyWage[@]}
