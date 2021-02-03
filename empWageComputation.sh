e77effe99b9c6873cfd0c77291c8b2d4159e77ed
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-03 17:28:20 +0530

    Added code for checking Daily Wage of an Employee.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,10 +4,15 @@ echo "Welcome to Employee Wage Computation Program!"
 
 isPresent=1;
 randomCheck=$((RANDOM%2));
+full_day_hour=8;
+wage_per_hour=20;
 
 if [[ $isPresent -eq $randomCheck ]]
 then
         echo "Employee is Present";
+	empWage=$(($wage_per_hour*$full_day_hour));
+        echo "Employee wage is : " $empWage;
 else
         echo "Employee is not Present"
+        echo "Employee wage is : 0";
 fi
