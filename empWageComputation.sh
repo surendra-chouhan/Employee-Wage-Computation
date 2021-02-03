ea46740803fb81e7c92b60e28b17955849ddfd5d
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-03 17:57:27 +0530

    Refactored the code by using Switch Case Statements instead of if-else.

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,21 +2,21 @@
 
 echo "Welcome to Employee Wage Computation Program!"
 
-randomCheck=$((RANDOM%3));
+empCheck=$((RANDOM%3));
 wage_per_hour=20;
 isPartTime=1;
 isFullTime=2;
 
-if [[ $isFullTime -eq $randomCheck ]]
-then
-        echo "Employee is FullTime";
-	emp_hour=8;
-elif [[ $isPartTime -eq $randomCheck ]]
-then
-	echo "Employee is PartTime";
-	emp_hour=4
-else
-        echo "Employee is Absent";
-	emp_hour=0;
-fi
+case $empCheck in
+	$isFullTime)
+		emp_hour=8
+		;;
+	$isPartTime)
+		emp_hour=4
+		;;
+	*)
+		emp_hour=0;
+		;;
+esac
+
 echo "Employee Wage is : " $(($emp_hour*$wage_per_hour));
