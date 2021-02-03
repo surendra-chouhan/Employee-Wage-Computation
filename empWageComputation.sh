2775ff0f6a2e39049f26b3542d91ab78670855fd
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-03 19:22:36 +0530

    Added code to check Monthly Wages of an Employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,25 +6,28 @@ wage_per_hour=20;
 isPartTime=1;
 isFullTime=2;
 No_of_working_days=20
+max_hours=100;
+
 Monthly_Wage=0;
 
-for (( day=1; day<=$No_of_working_days; day++ ))
-do
-	empCheck=$((RANDOM%3));
-	case $empCheck in
-		$isFullTime)
-			emp_hour=8
-			;;
-		$isPartTime)
-			emp_hour=4
-			;;
-		*)
-			emp_hour=0;
-			;;
-	esac
+empCheck=$((RANDOM%3));
+case $empCheck in
+	$isFullTime)
+		emp_hour=8
+		;;
+	$isPartTime)
+		emp_hour=4
+		;;
+	*)
+		emp_hour=0;
+		;;
+esac
 
-	Salary=$(( $emp_hour * $wage_per_hour ));
-	Monthly_Wage=$(( $Monthly_Wage + $Salary ));
+while [[ $total_working_days -lt $No_of_working_days ]]
+do
+	((total_working_days++))
+	total_work_hours=$(($total_work_hours+$emp_hour))
 done
 
-echo "Monthly Wage is : " $Monthly_Wage
+total_salary=$(( $total_work_hours * $wage_per_hour ))
+echo "Monthly Wage of an Employee is : " $total_salary
